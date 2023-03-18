95c3db1705c3a4355fdc658e21cbf5e0635dded5
Isaev Mustafa <mustafaisaev3@gmail.com>
2023-03-19 00:29:30 +0300

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="title">Hello from Ecommerce Admin)</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.title {
+    font-size: 50px;
+    color: rgb(50, 204, 255);
+}
+</style>
